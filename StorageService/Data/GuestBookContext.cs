28f5881b12728b71e1ec56002aa38cd0d8bec4b2
Anatoly Samokisha <anatoly@samokisha.com>
2021-04-14 15:58:57 +0300

    Add change tracker to set comment cretion datetime

--- StorageService/Data/GuestBookContext.cs
+++ StorageService/Data/GuestBookContext.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using StorageService.Model; // TODO: Maybe better using MessageContract
 
 namespace StorageService.Data
@@ -18,6 +23,19 @@ namespace StorageService.Data
                 .HasConversion<string>();
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            ChangeTracker.DetectChanges();
+            
+            ChangeTracker
+                .Entries<Comment>()
+                .Where(e => e.State == EntityState.Added)
+                .ToList()
+                .ForEach(e => e.Entity.CreatedAt = DateTime.UtcNow);
+            
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
